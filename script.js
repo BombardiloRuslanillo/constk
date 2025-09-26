00254373c71d1686a03c23160bbd481e5af19dd3
BombardiloRuslanillo <ruslanjolmuradov@gmail.com>
2025-09-26 10:46:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+  const arr1 = [2, 4, 9, 10, 45, 67, 8, 90];
+  let arrayebk = ('Boshlanishdagi array')
+console.log(arr1,arrayebk);
+    const arr2 = [2, 4, 10, 8, 90];
+      let arra2 = ('Juft sonlarga ajratilgan array')
+    console.log(arr2,arra2);
+    
+
